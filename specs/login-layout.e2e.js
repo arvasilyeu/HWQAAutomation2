a615b55fa3f098e297446769310bafa88899c1cb
ArtsiomVasilyeu <vslvrtm@gmail.com>
2021-11-18 23:29:14 +0300

    Commit message.

--- specs/login-layout.e2e.js
+++ specs/login-layout.e2e.js
@@ -1,17 +1,23 @@
-// REMOVE THE BELOW CODE BEFORE START THE EXERCISE
 describe('Check app', function () {
     it('should login', async function () {
         await browser.url('https://viktor-silakov.github.io/course-sut');
         await $('#login').setValue('walker@jw.com');
         await $('#password').setValue('password');
         await $('button').click();
-        await $('#spinner').waitForDisplayed({ reverse: false, timeout: 5000 });
-        await browser.pause(15000);
+        await $('#spinner').waitForDisplayed({ reverse: true, timeout: 15000 });
         const title = await browser.getTitle();
         if (title !== 'Report portal') {
             throw new Error('You don`t login into system!!!')
         }
     });
+    it('check items background color', async function () {
+        const sidebarItems = await $$('#first-nav-block > li')
+        for (const item of sidebarItems) {
+            await item.moveTo(5, 5)
+            const color = await item.getCSSProperty('background-color')
+            if (color.value == 'rgba(255,0,0,1)') {
+                throw new Error(`The menu Item "${await item.getText()}" has wrong color!`)
+            }
+        }
+    })    
 });
-
-
